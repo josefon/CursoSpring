69ebc4851ff90b2c49d9e2a329e9a4a1c55c2be2
josefon <Win7_64@10.0.2.15>
2016-11-30 22:16:48 +0100

    Se añaden clases SIN Spring

---
+++ spring-interprete-01/src/main/java/prueba/PruebaInterprete.java
@@ -0,0 +1,16 @@
+package prueba;
+
+import beans.InterpreteEspanol;
+import beans.Traductor;
+
+public class PruebaInterprete {
+    public static void main(String args[]){
+        Traductor traductor = new Traductor();
+        InterpreteEspanol interprete = new InterpreteEspanol();
+        
+        traductor.setInterprete(interprete);
+        traductor.setNombre("Carlos Esparza");
+        traductor.hablar();
+        
+    }
+}
